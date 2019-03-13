a571f12bb34aa628ffd900cb8de00e0023dd1313
Edvaldo da Rosa <fraterblack@bck.com.br>
2019-03-13 20:20:44 -0300

    Initial commit

---
+++ src/br/com/nocaute/database/ConnectionFactory.java
@@ -0,0 +1,33 @@
+package br.com.nocaute.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	public static Connection getConnection(
+			final String dbName, 
+			final String username, 
+			final String password
+	) {
+		return getConnection("localhost", "5432", dbName, username, password);
+	}
+	
+	public static Connection getConnection(
+			final String dbHost, 
+			final String port, 
+			final String dbName, 
+			final String username, 
+			final String password
+	) {
+		Connection conn = null;
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:postgresql://" + dbHost + ":" + port + "/" + dbName, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
---
+++ src/br/com/nocaute/test/DatabaseConnectionTest.java
@@ -0,0 +1,20 @@
+package br.com.nocaute.test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.nocaute.database.ConnectionFactory;
+
+public class DatabaseConnectionTest {
+public static void main(String[] args) {
+		
+		Connection conn = ConnectionFactory.getConnection("master", "admin", "admin");
+		
+		try {
+			conn.setAutoCommit(false);
+			System.out.println("Conectado com sucesso!!!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
