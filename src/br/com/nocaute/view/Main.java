582e64deb15cee9144445cd4c9b2b23ec11f1c8a
Edvaldo da Rosa <fraterblack@bck.com.br>
2019-04-24 20:45:02 -0300

    Corrige estilo da tela no Windows

--- src/br/com/nocaute/view/Main.java
+++ src/br/com/nocaute/view/Main.java
@@ -1,43 +1,47 @@
 package br.com.nocaute.view;
 
+import javax.swing.UIManager;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		//Muda o Layout da janela para o padrão do Windows
-		 try {
-	            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-	            	//Windows
-	            	if ("Windows".equals(info.getName())) {
-	                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-	                    break;
-	                }
-	            	//Linux
-	            	if ("Nimbus".equals(info.getName())) {
-	            		javax.swing.UIManager.setLookAndFeel(info.getClassName());
-	                    break;
-	                }
+		// Muda o Layout da janela para o padrão do Windows
+		try {
+			boolean hasWindowLookAndFeel = false;
+			
+			for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				// Windows
+				if ("Windows".equals(info.getName())) {
+					hasWindowLookAndFeel = true;
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+			
+			//Alternativa para outros sistemas operacionais
+			if (!hasWindowLookAndFeel) {
+				UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		}
 
-	            }
-	        } catch (ClassNotFoundException ex) {
-	            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	        } catch (InstantiationException ex) {
-	            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	        } catch (IllegalAccessException ex) {
-	            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-	            java.util.logging.Logger.getLogger(Window.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	        }
+		java.awt.EventQueue.invokeLater(new Runnable() {
 
-	        java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
 
-	            public void run() {
-	            	
-	            	new LoginWindow().setVisible(true);
-	            	//Para testes
-	                //new Window().setVisible(true);
-	            }
-	        });
+				new LoginWindow().setVisible(true);
+				// Para testes
+				// new Window().setVisible(true);
+			}
+		});
 	}
 
 }
