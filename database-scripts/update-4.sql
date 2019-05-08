4a7bd1fc597de8b0e130b5cd7fe2a65c029e4a04
Edvaldo da Rosa <fraterblack@bck.com.br>
2019-05-08 20:28:20 -0300

    Organizando backup banco de dados

--- database-scripts/update-4.sql
+++ database-scripts/update-4.sql
@@ -2,5 +2,3 @@ TRUNCATE assiduidade CASCADE;
 TRUNCATE faturas_matriculas CASCADE;
 TRUNCATE matriculas_modalidades CASCADE;
 TRUNCATE matriculas CASCADE;
-
-ALTER TABLE faturas_matriculas ADD COLUMN quantidade_modalidade INTEGER;
