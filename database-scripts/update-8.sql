cef6245b0f65f3344234fb5d3dc3afbdf46d486a
Leacina <giovaneleacina@unesc.net>
2019-06-15 22:45:43 -0300

    Alteração nome função para alterar data

--- database-scripts/update-8.sql
+++ database-scripts/update-8.sql
@@ -1,4 +1,4 @@
-create  or replace function data_alterecao()
+create  or replace function data_alteracao()
 returns trigger as $$
 begin
 	new.ultima_atualizacao = current_timestamp;
@@ -10,7 +10,7 @@ create trigger tgr_usuarios_after_u
 	before update
 	ON usuarios
 	FOR EACH ROW
-EXECUTE procedure data_alterecao();
+EXECUTE procedure data_alteracao();
 
 create trigger tgr_alunos_after_u
 	before update
@@ -22,47 +22,47 @@ create trigger tgr_assiduidade_after_u
 	before update
 	ON assiduidade
 	FOR EACH ROW
-EXECUTE procedure data_alterecao();
+EXECUTE procedure data_alteracao();
 
 create trigger tgr_cidades_after_u
 	before update
 	ON cidades
 	FOR EACH ROW
-EXECUTE procedure data_alterecao();
+EXECUTE procedure data_alteracao();
 
 create trigger tgr_faturas_matriculas_after_u
 	before update
 	ON faturas_matriculas 
 	FOR EACH ROW
-EXECUTE procedure data_alterecao();
+EXECUTE procedure data_alteracao();
 
 create trigger tgr_graduacoes_after_u
 	before update
 	ON graduacoes
 	FOR EACH ROW
-EXECUTE procedure data_alterecao();
+EXECUTE procedure data_alteracao();
 
 create trigger tgr_matriculas_after_u
 	before update
 	ON matriculas
 	FOR EACH ROW
-EXECUTE procedure data_alterecao();
+EXECUTE procedure data_alteracao();
 
 create trigger tgr_matriculas_modalidades_after_u
 	before update
 	ON matriculas_modalidades
 	FOR EACH ROW
-EXECUTE procedure data_alterecao();
+EXECUTE procedure data_alteracao();
 
 create trigger tgr_modalidades_after_u
 	before update
 	ON modalidades
 	FOR EACH ROW
-EXECUTE procedure data_alterecao();
+EXECUTE procedure data_alteracao();
 
 
 create trigger tgr_planos_after_u
 	before update
 	ON planos
 	FOR EACH ROW
-EXECUTE procedure data_alterecao();
+EXECUTE procedure data_alteracao();
